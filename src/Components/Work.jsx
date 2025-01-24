199237ecb5e6163275b7c98d37be53e603c4f7a1
ZAINZESHAN <153626862+ZAINZESHAN@users.noreply.github.com>
2025-01-24 23:36:55 +0500

    ok

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import work_img1 from '../assets/work-1.jfif';
 import work_img2 from '../assets/work-2.jfif';
 import work_img3 from '../assets/work-3.jfif';
@@ -8,6 +8,7 @@ import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
 import WorkSliderBtns from './WorkSliderBtns';
+import ScrollReveal from 'scrollreveal';
 
 const slideData = [
   { index: '01', title: 'Portfolio Website', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, labore a explicabo.', skills: 'Html 5, Css 3, Javascript', img: work_img1 },
@@ -17,6 +18,7 @@ const slideData = [
 ];
 
 const Work = () => {
+
   const [icon, setIcon] = useState('fa-solid fa-arrow-right-long');
   const [headingColor, setHeadingColor] = useState('white');
   const [skillColor, setSkillColor] = useState('accent');
@@ -33,19 +35,35 @@ const Work = () => {
     }
   };
 
+  useEffect(() => {
+    const sr = ScrollReveal({
+      origin: 'left',
+      distance: '60px',
+      duration: 2500,
+      delay: 300,
+      reset: true
+    })
+
+    sr.reveal(`.work_heading`, { origin: 'top' });
+    sr.reveal(`.work_content`);
+    sr.reveal(`.work_image`, { origin: 'right' });
+  }, []);
+
+
   return (
     <section id='work' className='bg-black'>
       <div className='container py-10 text-white'>
         <div className='container py-10 text-white text-center'>
-          <h1 className='font-Viga text-white pb-3 text-[30px] md:text-[40px] lg:text-[32px] xl:text-[50px]'>
-            My Work
-            <span className='text-4xl md:text-3xl lg:text-4xl xl:text-5xl text-accent pl-1'>.</span>
-          </h1>
-          <p className='text-[14px] md:text-[16px] font-semibold lg:text-[18px] xl:text-[19px] text-gray-200'>
-            Here you will find more information about me, my journey as a developer, my passion for crafting innovative solutions, and the skills I bring to the table.
-          </p>
+          <div className='work_heading'>
+            <h1 className='font-Viga text-white pb-3 text-[30px] md:text-[40px] lg:text-[32px] xl:text-[50px]'>
+              My Work
+              <span className='text-4xl md:text-3xl lg:text-4xl xl:text-5xl text-accent pl-1'>.</span>
+            </h1>
+            <p className='text-[14px] md:text-[16px] font-semibold lg:text-[18px] xl:text-[19px] text-gray-200'>
+              Here you will find insights into my projects, the challenges I’ve tackled, the technologies I’ve employed, and the impact of my contributions in the realm of development.          </p>
+          </div>
 
-          <div className='pt-10 md:pt-16'>
+          <div className='pt-10 md:pt-28'>
             <style>
               {`
                 .swiper-pagination-bullet {
@@ -69,7 +87,7 @@ const Work = () => {
               {slideData.map(({ index, title, description, skills, img }) => (
                 <SwiperSlide key={index} className='pb-1 lg:pb-10 xl:pb-14'>
                   <div className='text-left flex h-[42vh] md:h-[54vh] lg:h-auto lg:flex-row flex-col lg:justify-between md:gap-5 lg:gap-10 xl:gap-20 relative'>
-                    <div className='lg:w-[50%]'>
+                    <div className='work lg:w-[50%]'>
                       <h1 className='text-[40px] md:text-[50px] lg:text-[50px] xl:text-[70px] font-Bungee'>{index}</h1>
                       <h1 className={`font-Viga text-${headingColor} text-[26px] md:text-[30px] lg:text-[25px] xl:text-[45px] mt-0 lg:mt-[-10px] lg:pb-[6px] xl:pb-[10px]`}>
                         {title}
@@ -90,7 +108,7 @@ const Work = () => {
                         <i className='text-[20px] md:text-[25px] lg:text-[20px] xl:text-[30px] p-2 xl:p-3 bg-[#141414] rounded-full cursor-pointer hover:text-accent ri-github-fill'></i>
                       </div>
                     </div>
-                    <div className='lg:w-[50%]'>
+                    <div className='work lg:w-[50%]'>
                       <img src={img} alt='Project Image' className='w-[100%] h-[100%]' />
                     </div>
                   </div>
